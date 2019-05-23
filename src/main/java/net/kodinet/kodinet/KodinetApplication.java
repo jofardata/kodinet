25eba73da5075230eeb027482342fa8e2a104e51
nationaldata <nationaldatasystem@gmail.com>
2019-05-23 11:08:15 +0200

    project creation

---
+++ src/main/java/net/kodinet/kodinet/KodinetApplication.java
@@ -0,0 +1,13 @@
+package net.kodinet.kodinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodinetApplication.class, args);
+	}
+
+}
