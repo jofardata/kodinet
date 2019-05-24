da6b972e06a4e33e1c1d1d9e8ec6c1a842fa263d
nationaldata <nationaldatasystem@gmail.com>
2019-05-24 10:29:55 +0200

    Person profile update

--- src/main/java/net/kodinet/kodinet/entities/Asset.java
+++ src/main/java/net/kodinet/kodinet/entities/Asset.java
@@ -3,12 +3,16 @@ package net.kodinet.kodinet.entities;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity(name = "assets")
 public class Asset {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private Long latitude;
