2fb41f561b40535c9b7865e155f531de0ede4bfc
nationaldata <nationaldatasystem@gmail.com>
2019-05-29 13:23:43 +0200

    intergrating admin login from the admin portal

--- src/main/java/net/kodinet/kodinet/repositories/AdminRepository.java
+++ src/main/java/net/kodinet/kodinet/repositories/AdminRepository.java
@@ -2,8 +2,10 @@ package net.kodinet.kodinet.repositories;
 
 import net.kodinet.kodinet.entities.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
+    @Query(value = "select * from admins where username =?1", nativeQuery = true)
     Admin findByUsername(String username);
 }
