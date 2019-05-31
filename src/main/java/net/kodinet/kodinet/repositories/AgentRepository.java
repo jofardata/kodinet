fd639cbc8df69141c2b066b9905794f68f996475
nationaldata <nationaldatasystem@gmail.com>
2019-05-31 09:37:23 +0200

    fetch all agents

--- src/main/java/net/kodinet/kodinet/repositories/AgentRepository.java
+++ src/main/java/net/kodinet/kodinet/repositories/AgentRepository.java
@@ -2,8 +2,15 @@ package net.kodinet.kodinet.repositories;
 
 import net.kodinet.kodinet.entities.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AgentRepository extends JpaRepository<Agent, Long> {
+
+    @Query(value = "select * from agents order by id desc ", nativeQuery = true)
+    @Override
+    List<Agent> findAll();
 
     Agent findByUsername(String username);
 }
