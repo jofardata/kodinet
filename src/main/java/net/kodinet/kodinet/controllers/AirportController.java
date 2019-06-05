e776f8966ababf9a9cb8f5f5d60ccf19389c1611
nationaldata <nationaldatasystem@gmail.com>
2019-06-05 13:09:46 +0200

    bug fix

--- src/main/java/net/kodinet/kodinet/controllers/AirportController.java
+++ src/main/java/net/kodinet/kodinet/controllers/AirportController.java
@@ -1,8 +1,10 @@
 package net.kodinet.kodinet.controllers;
 
 import net.kodinet.kodinet.entities.Airport;
+import net.kodinet.kodinet.entities.Town;
 import net.kodinet.kodinet.models.ApiResponse;
 import net.kodinet.kodinet.repositories.AirportRepository;
+import net.kodinet.kodinet.repositories.TownRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,15 @@ public class AirportController {
 
     @Autowired
     AirportRepository airportRepository;
+    @Autowired
+    TownRepository townRepository;
     ApiResponse apiResponse = new ApiResponse();
 
-    @PostMapping("/create")
-    public ResponseEntity<?>create(@RequestBody Airport airport){
+    @PostMapping("/create/{townId}")
+    public ResponseEntity<?>create(@RequestBody Airport airport,@PathVariable Long townId){
 
+        Town town = townRepository.getOne(townId);
+        airport.setTown(town);
         airportRepository.save(airport);
         apiResponse.setResponseCode("00");
         apiResponse.setResponseMessage("Airport Created");
