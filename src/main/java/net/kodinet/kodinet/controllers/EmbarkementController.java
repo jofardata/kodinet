629b74bbb7c74ebca45e84d80efe053a318d0d2e
nationaldata <nationaldatasystem@gmail.com>
2019-06-05 08:19:18 +0200

    saving embarkement

--- src/main/java/net/kodinet/kodinet/controllers/EmbarkementController.java
+++ src/main/java/net/kodinet/kodinet/controllers/EmbarkementController.java
@@ -1,7 +1,9 @@
 package net.kodinet.kodinet.controllers;
 
+import net.kodinet.kodinet.entities.Agent;
 import net.kodinet.kodinet.entities.Embarkment;
 import net.kodinet.kodinet.models.ApiResponse;
+import net.kodinet.kodinet.repositories.AgentRepository;
 import net.kodinet.kodinet.repositories.EmbarkementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,18 +16,24 @@ public class EmbarkementController {
 
     @Autowired
     EmbarkementRepository embarkementRepository;
+    @Autowired
+    AgentRepository agentRepository;
     ApiResponse apiResponse = new ApiResponse();
 
-    @PostMapping("/create")
-    public ResponseEntity<?>create(@RequestBody Embarkment embarkment){
+    @PostMapping("/create/{agentId}")
+    public ResponseEntity<?>create(@RequestBody Embarkment embarkment,
+                                   @PathVariable Long agentId){
+        Agent agent = agentRepository.getOne(agentId);
+        embarkment.setAgent(agent);
         apiResponse.setResponseCode("00");
         apiResponse.setResponseMessage("Embarkement created");
         embarkementRepository.save(embarkment);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
-    
+
     @GetMapping("/find-all")
     public ResponseEntity<?>findAll(){
+
         apiResponse.setResponseCode("00");
         apiResponse.setData(embarkementRepository.findAll());
 
