aff2816913e7f56997661b0b30681a8775424ee6
nationaldata <nationaldatasystem@gmail.com>
2019-07-29 16:05:14 +0200

    fiscal administration

--- src/main/java/net/kodinet/kodinet/controllers/FiscalAdministrationController.java
+++ src/main/java/net/kodinet/kodinet/controllers/FiscalAdministrationController.java
@@ -1,10 +1,40 @@
 package net.kodinet.kodinet.controllers;
 
+import net.kodinet.kodinet.entities.FiscalAdministration;
+import net.kodinet.kodinet.models.ApiResponse;
+import net.kodinet.kodinet.repositories.FiscalAdministrationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/fiscal-entity")
+@RequestMapping("/fiscal-administration")
 public class FiscalAdministrationController {
 
+    @Autowired
+    FiscalAdministrationRepository fiscalAdministrationRepository;
+
+    ApiResponse apiResponse = new ApiResponse();
+
+    @GetMapping("/find-all")
+    public ResponseEntity<?> ReadAll(){
+        apiResponse = new ApiResponse();
+
+        try {
+            List<FiscalAdministration> fiscalAdministrations = fiscalAdministrationRepository.findAll();
+            apiResponse.setResponseCode("00");
+            apiResponse.setData(fiscalAdministrations);
+        } catch (Exception ex){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage(ex.getMessage());
+        }
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
 }
